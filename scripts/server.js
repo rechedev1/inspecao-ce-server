45e065a3725fb1aa7f7c5e62847ad76ccb820109
rechedev1 <cleyton11reche@gmail.com>
2025-05-25 16:47:30 -0300

    port 3000

--- scripts/server.js
+++ scripts/server.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const uploadRouter = require("../routes/upload");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Servir HTML na raiz
 app.get("/", (req, res) => {
-  res.send("API online");
+  res.sendFile(path.join(__dirname, "../index.html"));
 });
+
 // Servir JS e CSS
 app.get("/scripts/index.js", (req, res) => {
   res.sendFile(path.join(__dirname, "index.js"));

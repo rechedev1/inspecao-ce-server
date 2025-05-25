aaa3f43e9398979ebb1ea6eb26586bb0e66d4f1b
rechedev1 <cleyton11reche@gmail.com>
2025-05-25 16:44:13 -0300

    port 3000

--- scripts/index.js
+++ scripts/index.js
@@ -23,7 +23,7 @@ document.getElementById("uploadForm").addEventListener("submit", async (e) => {
   }
 
   try {
-    const response = await fetch("http://localhost:8080/upload", {
+    const response = await fetch("http://localhost:3000/upload", {
       method: "POST",
       body: formData,
     });
--- scripts/server.js
+++ scripts/server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 const uploadRouter = require("../routes/upload");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir HTML na raiz
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.send("API online");
 });
-
 // Servir JS e CSS
 app.get("/scripts/index.js", (req, res) => {
   res.sendFile(path.join(__dirname, "index.js"));
